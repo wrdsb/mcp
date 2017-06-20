bfd54045c179fe36b770d4711301edc3a282c4b4
James Schumann <jschumann@gmail.com>
2017-06-20 08:23:00 -0400

    Update schools.js

--- schools.js
+++ schools.js
@@ -1,15 +1,17 @@
 // get all schools
-// foreach schools as a_school:
+// foreach schools as the_school:
 
-a_school.should.have.a(staff_group);
-a_school.should.have.an(admin_group);
+the_school.should.have.a(staff_group);
+the_school.should.have.an(admin_group);
 
-a_school.should.have.a(public_calendar);
+the_school.should.have.a(public_calendar);
+  // assign calendar to the_public_calendar
   the_public_calendar.should.be.shared_publicly;
   the_public_calendar.should.be.readable_by(the_staff_group);
   the_public_calendar.should.be.editable_by(the_admin_group);
 
-a_school.should.have.a(staff_calendar);
+the_school.should.have.a(staff_calendar);
+  // assign calendar to the_staff_calendar
   the_staff_calendar.should.not.be.shared_publicly;
   the_staff_calendar.should.be.readable_by(the_staff_group);
   the staff_calendar.should.be.editable_by(the_admin_group);
