dffad2c80f765fddddc06aa3e8c5a2fce923f36a
James Schumann <jschumann@gmail.com>
2017-06-20 08:15:32 -0400

    Create schools.js

---
+++ schools.js
@@ -0,0 +1,8 @@
+// get all schools
+// foreach schools as a_school:
+
+a_school.should.have.a(staff_group);
+a_school.should.have.an(admin_group);
+
+a_school.should.have.a(public_calendar);
+a_school.should.have.a(private_calendar);
