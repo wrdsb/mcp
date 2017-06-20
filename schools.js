ebc1907f1c439d50865cfc1a97503aa514d24676
James Schumann <jschumann@gmail.com>
2017-06-20 08:21:16 -0400

    Update schools.js

--- schools.js
+++ schools.js
@@ -5,4 +5,11 @@ a_school.should.have.a(staff_group);
 a_school.should.have.an(admin_group);
 
 a_school.should.have.a(public_calendar);
-a_school.should.have.a(private_calendar);
+  the_public_calendar.should.be.shared_publicly;
+  the_public_calendar.should.be.readable_by(the_staff_group);
+  the_public_calendar.should.be.editable_by(the_admin_group);
+
+a_school.should.have.a(staff_calendar);
+  the_staff_calendar.should.not.be.shared_publicly;
+  the_staff_calendar.should.be.readable_by(the_staff_group);
+  the staff_calendar.should.be.editable_by(the_admin_group);
