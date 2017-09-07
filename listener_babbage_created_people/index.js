7823a9acdffbec1d5023b72ad2bcb45a4e0c8b80
James Schumann <jschumann@gmail.com>
2017-09-07 19:10:33 +0000

    stub listener_babbage_created_people

---
+++ listener_babbage_created_people/index.js
@@ -0,0 +1,23 @@
+module.exports = function (context, message) {
+    context.log(message);
+    // add new person to all WRDSB groups
+    // add new person to all WRDSB calendars
+    // add new person to Staff Intranet
+
+    // add new person to school groups?
+    // add new person to school calendars?
+    // add new person to school websites?
+
+    // add new person to dept groups?
+    // add new person to dept calendars?
+    // add new person to dept websites?
+
+    // add new person to job groups?
+    // add new person to job calendars?
+    // add new person to job websites?
+
+    // add new person to guide groups?
+    // add new person to guide calendars?
+    // add new person to guide websites?
+    context.done();
+};
